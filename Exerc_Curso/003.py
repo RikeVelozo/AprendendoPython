e3d235cbbbf5a154f40a31c676f9f04129c53e72
Henrique Velozo <164663481+RikeVelozo@users.noreply.github.com>
2026-02-12 20:07:17 +0000

    Realizacao do exercicio 3

---
+++ Exerc_Curso/003.py
@@ -0,0 +1,12 @@
+''' esse é um exemplo de que tipos primitivos são importante. Nesse caso está recebendo os números como strings
+ por causa disso está concatenando e não somando'''
+
+# num1 = input('digite um número ')
+# num2 = input('digite mais um número ')
+# soma = num1+num2
+# print (f'{num1} + {num2} é igual a {soma} ')
+
+num1 = int(input('Digite um número '))
+num2 = int(input('Digite outro número '))
+soma = num1+num2
+print  (f'O numero {num1} somando com o {num2} resulta no {soma}')
