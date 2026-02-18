4b45a6f05fb36fd830c6e8e7486f992cb0af6657
Henrique Velozo <164663481+RikeVelozo@users.noreply.github.com>
2026-02-18 19:25:26 +0000

    Começo de exercicio sobre arrays

---
+++ ExerciciosAParte/MediaAlunos.py
@@ -0,0 +1,11 @@
+#esse vai ser um sistema para registrar alunos, suas notas e as médias.
+
+array = ["name1", "name2", "name3"]
+array[0] = "Henrique"
+first_value = array[0]
+print (first_value)
+print(len(array))
+#adiciona valor no array
+ValorAdc = input ("Digite um valor: ")
+array.append(ValorAdc)
+print(len(array))
