cae583ffd5fa5aac95f9cdb63637a1107a5db8a9
Henrique Velozo <164663481+RikeVelozo@users.noreply.github.com>
2026-03-19 20:40:32 +0000

    Versão funcional com 1 aluno

--- ExerciciosAParte/MediaAlunos.py
+++ ExerciciosAParte/MediaAlunos.py
@@ -5,8 +5,10 @@ for i in range(1):
     aluno_indiv = []
     Nome = input('Digite o nome do aluno: ')
     aluno_indiv.append(Nome)
-    for j in range(2):
+    for j in range(3):
         Nota = int(input('Digite a nota do aluno: '))
         aluno_indiv.append(Nota)
+    media = int((aluno_indiv[1] + aluno_indiv[2] + aluno_indiv[3]) / 3)
     lista_alunos.append(aluno_indiv)
 print (lista_alunos)
+print (f"A média desse aluno é: {media}, ele foi {'APROVADO :)' if media >= 6 else 'REPROVADO :('}")
